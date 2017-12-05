7b9487a0bd5740470427519214212637629d27dd
smrk007 <skenne5@tigers.lsu.edu>
2017-12-05 13:53:56 -0600

    First commit message.

--- GUI_Debugger/main.cpp
+++ GUI_Debugger/main.cpp
@@ -1,15 +1,37 @@
-//
-//  main.cpp
-//  GUI_Debugger
-//
-//  Created by Sean Michael Ritschard Kennedy on 12/4/17.
-//  Copyright © 2017 skware. All rights reserved.
-//
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
 
-#include <iostream>
+#include <GL/glew.h>
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+ This program functions as a means to debug a graphics engine.
+*/
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2); // OpenGL 3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Make MacOS Happy
+    
+    // Opening a window
+    GLFWwindow* window;
+    window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+    if (window == NULL)
+    {
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        glfwTerminate();
+        return -1;
+    }
+    
     return 0;
 }
